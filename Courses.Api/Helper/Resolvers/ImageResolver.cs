a4e027ec679d9bb5b5a67f5777a2ebf89e1d1eeb
felosanad <felosanad@gmail.com>
2026-04-26 14:03:08 +0300

    Add ImageReslover Generic

---
+++ Courses.Api/Helper/Resolvers/ImageResolver.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+
+namespace Courses.Api.Helper.Resolvers
+{
+    public class ImageResolver<TSource, TDestination> : IMemberValueResolver<TSource, TDestination, string, string>
+    {
+        private readonly IConfiguration _config;
+
+        public ImageResolver(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public string Resolve(TSource source, TDestination destination, string sourceMember, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(sourceMember))
+                return $"{_config["BasePictureUrl"]}/{sourceMember}";
+            return string.Empty;
+        }
+    }
+}
