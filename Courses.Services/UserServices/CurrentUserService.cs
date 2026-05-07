b1e0878a7fb396da676c3b2b10308e5835289e46
felosanad <felosanad@gmail.com>
2026-05-07 14:39:44 +0300

    Add Current User Service

---
+++ Courses.Services/UserServices/CurrentUserService.cs
@@ -0,0 +1,28 @@
+﻿using Courses.Core.Services.Contract.UserServices;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Courses.Services.UserServices
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        protected readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? UserId => 
+            _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        public string? Email => 
+            _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Email);
+
+        public string? UserName =>
+            _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.GivenName);
+
+        public IList<string>? Roles =>
+            _httpContextAccessor.HttpContext?.User?.FindAll(ClaimTypes.Role).Select(r => r.Value).ToList();
+    }
+}
