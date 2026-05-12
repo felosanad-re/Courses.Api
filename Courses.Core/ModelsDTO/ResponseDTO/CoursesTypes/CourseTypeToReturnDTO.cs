c7d0f80532362fd668bd9f79b029785bdb9c269e
felosanad <felosanad@gmail.com>
2026-05-12 20:31:02 +0300

    Add Course type service

---
+++ Courses.Core/ModelsDTO/ResponseDTO/CoursesTypes/CourseTypeToReturnDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Courses.Core.ModelsDTO.ResponseDTO.CoursesTypes
+{
+    public class CourseTypeToReturnDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Courses.Core/Services.Contract/CourseTypeServices/ICourseTypeService.cs
@@ -0,0 +1,12 @@
+﻿using Courses.Core.ModelsDTO;
+using Courses.Core.ModelsDTO.ResponseDTO.CoursesTypes;
+
+namespace Courses.Core.Services.Contract.CourseTypeServices
+{
+    public interface ICourseTypeService
+    {
+        Task<ApplicationServiceResult<IReadOnlyList<CourseTypeToReturnDTO>>> GetAllTypesAsync();
+
+        // Add Course Type --> Instructor when create new course with new Type
+    }
+}
