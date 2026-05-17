0579892cf7454c9f8f7974a5e1ed1db77b8d4135
felosanad <felosanad@gmail.com>
2026-05-17 23:31:16 +0300

    Create Payment Controller

---
+++ Courses.Api/Controllers/Payment/PaymentController.cs
@@ -0,0 +1,42 @@
+﻿using Courses.Api.ErrorHandler;
+using Courses.Core.ModelsDTO;
+using Courses.Core.ModelsDTO.RequestDTO.Payments;
+using Courses.Core.ModelsDTO.ResponseDTO.Payments;
+using Courses.Core.Services.Contract.PaymentsServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Courses.Api.Controllers.Payment
+{
+    public class PaymentController : BaseController
+    {
+        protected readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        #region Create Payment
+        [HttpPost("CreatePaymentIntent")] // POST: /api/Payment/CreatePaymentIntent
+        public async Task<ActionResult<ApplicationServiceResult<PaymentResponse>>> CreatePayment(PaymentRequest req)
+        {
+            var result = await _paymentService.CreatePaymentIntent(req);
+            if (!result.Succeed) return BadRequest(new ErrorResponse(400) { Message = [result.Message] });
+
+            return Ok(result);
+        }
+        #endregion
+
+        #region Get Payment
+        [HttpGet("PaymentIntent")] // GET: /api/Payment/PaymentIntent
+        public async Task<ActionResult<ApplicationServiceResult<PaymentResponse>>> GetPayment(string paymentIntentId)
+        {
+            var result = await _paymentService.GetPaymentIntent(paymentIntentId);
+            if(!result.Succeed) return BadRequest(new ErrorResponse(400) { Message = [result.Message] });
+
+            return Ok(result);
+        }
+        #endregion
+    }
+}
