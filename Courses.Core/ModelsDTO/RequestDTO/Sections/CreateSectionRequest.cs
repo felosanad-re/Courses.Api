390d04fb83d31188ff83259f55c502d09e77b712
felosanad <felosanad@gmail.com>
2026-05-27 04:30:37 +0300

    Refactor In Section Services Add Some Validations

--- Courses.Core/ModelsDTO/RequestDTO/Sections/CreateSectionRequest.cs
+++ Courses.Core/ModelsDTO/RequestDTO/Sections/CreateSectionRequest.cs
@@ -5,12 +5,16 @@ namespace Courses.Core.ModelsDTO.RequestDTO.Sections
     public class CreateSectionRequest
     {
         [Required]
-        public string Title { get; set; }
+        [MinLength(3)]
+        [MaxLength(300)]
+        public string Title { get; set; } = string.Empty;
 
         [Required]
+        [Range(1, int.MaxValue)]
         public int Order { get; set; }
 
         [Required]
+        [Range(1, int.MaxValue)]
         public int CourseId { get; set; }
     }
 }
--- Courses.Core/ModelsDTO/RequestDTO/Sections/UpdateSectionRequest.cs
+++ Courses.Core/ModelsDTO/RequestDTO/Sections/UpdateSectionRequest.cs
@@ -4,11 +4,17 @@ namespace Courses.Core.ModelsDTO.RequestDTO.Sections
 {
     public class UpdateSectionRequest
     {
-        public int Id { get; set; }
         [Required]
-        public string Title { get; set; }
+        [Range(1, int.MaxValue)]
+        public int Id { get; set; }
 
         [Required]
+        [MinLength(3)]
+        [MaxLength(300)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, int.MaxValue)]
         public int Order { get; set; }
     }
 }
